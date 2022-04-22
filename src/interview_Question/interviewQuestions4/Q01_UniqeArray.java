1195be1b28d6b7bd44ef1bafa95ced0763cb0c40
mu5tafacool <mustafakulf1@gmail.com>
2022-04-22 22:29:16 +0300

    22 04 22 22:30

--- src/interview_Question/interviewQuestions4/Q01_UniqeArray.java
+++ src/interview_Question/interviewQuestions4/Q01_UniqeArray.java
@@ -23,6 +23,22 @@ public class Q01_UniqeArray {
             }
         }
         System.out.println(tekrarsizListe); //[1, 2, 3, 4, -2, 5, 6, 8, 7, 9, 10]
+
+
+        System.out.println("\n*************2 YOL SON RAKAM YOK????????*************");
+        ArrayList<Integer> out = new ArrayList<>(); //konteyner
+
+        for (int k = 0; k < arr.length; k++) {    //{1, 2, 2, 3, 1, 4, 2, -2, 5, 6, 8, 7, 5, 6, -2, 9, 3, 10}
+            for (int l = k; l < arr.length; l++) {//{1, 2, 2, 3, 1, 4, 2, -2, 5, 6, 8, 7, 5, 6, -2, 9, 3, 10};
+                if (arr[k] != arr[l] && !out.contains(arr[k])) {
+                    System.out.print(arr[k]); //1234-256879
+                    out.add(arr[k]); //output a ekle
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("out = " + out); //[1, 2, 3, 4, -2, 5, 6, 8, 7, 9]
     }
+
 
 }
--- src/interview_Question/interviewQuestions6/Q06_DuplicateValue.java
+++ src/interview_Question/interviewQuestions6/Q06_DuplicateValue.java
@@ -14,16 +14,16 @@ public class Q06_DuplicateValue {
          */
 
     public static void main(String[] args) {
-        String str="Javaisalsoeasy";
+        String str = "Javaisalsoeasy";
         char[] arr = str.toCharArray(); // her bir karakteri bir arraye donusturur
         System.out.println("Arrays.toString(arr) = " + Arrays.toString(arr));
         //Arrays.toString(arr) = [J, a, v, a, i, s, a, l, s, o, e, a, s, y]
 
         ArrayList<Character> output = new ArrayList<>(); //istenilen cikti icin
 
-        for (int i = 0; i <arr.length; i++){ //arr deki her bir elemana bakar
-            for (int j = i+1; j < arr.length; j++){ //her bir elemana bakarken bir sonraki ile karsilastir
-                if(arr[i] == arr[j] && !output.contains(arr[j])) { //ayni elemandan varsa ve istedigin output da yoksa
+        for (int i = 0; i < arr.length; i++) { //arr deki her bir elemana bakar
+            for (int j = i + 1; j < arr.length; j++) { //her bir elemana bakarken bir sonraki ile karsilastir
+                if (arr[i] == arr[j] && !output.contains(arr[j])) { //ayni elemandan varsa ve istedigin output da yoksa
                     //&& !output.contains(arr[j] == TRICK bu konteynirdaki eleman kontrolu icin
                     System.out.println(arr[j]); // o elemani yazdir
                     output.add(arr[j]); //output a ekle
