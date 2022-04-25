22531f56b411ac14dd716a917629ef5c3cfcd284
mu5tafacool <mustafakulf1@gmail.com>
2022-04-26 01:40:41 +0300

    26 04 22 01:40 Random_Integer_Change_String

---
+++ src/q_Format_Example/printf/Random_Integer_Change_String.java
@@ -0,0 +1,25 @@
+package src.q_Format_Example.printf;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Random_Integer_Change_String {
+
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        List<String> idStr = new ArrayList<>(); //bos id listi
+
+//ID atamalarinda kullanmak icin
+        String a = String.valueOf((rnd.nextInt(100))); //Random integer olan sayiyi String e cevirme
+        String b = String.valueOf((rnd.nextInt(100))); //Random integer olan sayiyi String e cevirme
+        String c = String.valueOf((rnd.nextInt(100))); //Random integer olan sayiyi String e cevirme
+
+        idStr.add(a);
+        idStr.add(b);
+        idStr.add(c);
+
+        System.out.println("a = " + a);
+        System.out.println("idStr = " + idStr);
+    }
+}
