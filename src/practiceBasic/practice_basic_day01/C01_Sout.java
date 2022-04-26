b695fcb5ac6c85a9cf969119db1d5689521a0437
mu5tafacool <mustafakulf1@gmail.com>
2022-04-26 22:06:48 +0300

    26 04 22 22:07

---
+++ src/practiceBasic/practice_basic_day01/C01_Sout.java
@@ -0,0 +1,23 @@
+package src.practiceBasic.practice_basic_day01;
+
+public class C01_Sout {
+    public static void main(String[] args) {
+        System.out.println("T");
+        System.out.println("E");
+        System.out.println("C");
+        System.out.println("H");
+        System.out.println("P");
+        System.out.println("R");
+        System.out.println("O");
+        System.out.println(" ");
+        System.out.println("E");
+        System.out.println("D");
+        System.out.println("U");
+        System.out.println("C");
+        System.out.println("A");
+        System.out.println("T");
+        System.out.println("I");
+        System.out.println("O");
+        System.out.println("N");
+    }
+}
