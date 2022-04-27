18fc5febb17bd48a38a17e863fd4725e190aa072
mu5tafacool <mustafakulf1@gmail.com>
2022-04-27 13:49:57 +0300

    27 04 22 13:50 Random_Integer_Change_String

--- src/q_Format_Example/printf/Random_Integer_Change_String.java
+++ src/q_Format_Example/printf/Random_Integer_Change_String.java
@@ -10,7 +10,8 @@ public class Random_Integer_Change_String {
         Random rnd = new Random();
         List<String> idStr = new ArrayList<>(); //bos id listi
 
-//ID atamalarinda kullanmak icin
+        //ID atamalarinda kullanmak icin
+        // Random ile ArrayList List e atama yapma
         String a = String.valueOf((rnd.nextInt(100))); //Random integer olan sayiyi String e cevirme
         String b = String.valueOf((rnd.nextInt(100))); //Random integer olan sayiyi String e cevirme
         String c = String.valueOf((rnd.nextInt(100))); //Random integer olan sayiyi String e cevirme
@@ -19,7 +20,16 @@ public class Random_Integer_Change_String {
         idStr.add(b);
         idStr.add(c);
 
-        System.out.println("a,b,c = " + a+", "+b+", "+c);
+        System.out.println("a,b,c = " + a + ", " + b + ", " + c);
         System.out.println("idStr = " + idStr);
+
+
+        /*
+         Random rand = new Random();
+        for (int i = 0; i < 30; i++) {
+            list.add(rand.nextInt(10));
+
+        }
+        */
     }
 }
