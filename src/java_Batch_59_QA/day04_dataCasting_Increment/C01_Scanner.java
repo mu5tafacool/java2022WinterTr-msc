a7060a47cddaf07676fa45b0dd00c2233a21efb6
mu5tafacool <mustafakulf1@gmail.com>
2022-05-01 00:37:39 +0300

    01 05 22 00:38

--- src/java_Batch_59_QA/day04_dataCasting_Increment/C01_Scanner.java
+++ src/java_Batch_59_QA/day04_dataCasting_Increment/C01_Scanner.java
@@ -1,4 +1,4 @@
-package day04_dataCasting_Increment;
+package src.java_Batch_59_QA.day04_dataCasting_Increment;
 import java.util.Scanner;
 public class C01_Scanner {
     public static void main(String[] args) {
