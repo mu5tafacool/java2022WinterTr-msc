072a2e321a23bfc87bae09fc170b82873ed4735d
mu5tafacool <mustafakulf1@gmail.com>
2022-05-05 00:07:01 +0300

    05 05 22 00:07

---
+++ src/Proje/P11_depoYonetimi/depoYonetimi04/DepoMain.java
@@ -0,0 +1,10 @@
+package src.Proje.P11_depoYonetimi.depoYonetimi04;
+
+import static src.Proje.P11_depoYonetimi.depoYonetimi04.Islemler.secim;
+
+public class DepoMain {
+    public static void main(String[] args) {
+
+        secim();
+    }
+}
---
+++ src/Proje/P11_depoYonetimi/depoYonetimi04/Satis.java
@@ -0,0 +1,5 @@
+package src.Proje.P11_depoYonetimi.depoYonetimi04;
+
+public interface Satis {
+    public abstract void urunCikis(int id, int miktar);
+}
