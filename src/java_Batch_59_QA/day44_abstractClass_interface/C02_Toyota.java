1015651f3615e2d55b38109e8c3cec1fe33df1b5
mu5tafacool <mustafakulf1@gmail.com>
2022-05-12 22:08:44 +0300

    12 05 22 22:09

---
+++ src/java_Batch_59_QA/day44_abstractClass_interface/C02_Toyota.java
@@ -0,0 +1,9 @@
+package src.java_Batch_59_QA.day44_abstractClass_interface;
+public class C02_Toyota extends C01_AbstractClass{
+    @Override
+    public void motor() {
+    }
+    @Override
+    public void vites() {
+    }
+}
