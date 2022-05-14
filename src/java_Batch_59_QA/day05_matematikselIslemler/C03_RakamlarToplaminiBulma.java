50ae1eef5c469f935a6bfeab209e0230bacbde09
mu5tafacool <mustafakulf1@gmail.com>
2022-05-15 02:16:51 +0300

    15 05 22 02:17

--- src/java_Batch_59_QA/day05_matematikselIslemler/C03_RakamlarToplaminiBulma.java
+++ src/java_Batch_59_QA/day05_matematikselIslemler/C03_RakamlarToplaminiBulma.java
@@ -1,4 +1,4 @@
-package day05_matematikselIslemler;
+package src.java_Batch_59_QA.day05_matematikselIslemler;
 import java.util.Scanner;
 public class C03_RakamlarToplaminiBulma {
     public static void main(String[] args) {
