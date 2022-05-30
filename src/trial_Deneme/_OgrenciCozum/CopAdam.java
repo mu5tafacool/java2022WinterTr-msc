0bf412de697af90d21dc29394f07631f2b4f8615
mu5tafacool <mustafakulf1@gmail.com>
2022-05-30 19:35:13 +0300

    30 05 22 19:35

---
+++ src/trial_Deneme/_OgrenciCozum/CopAdam.java
@@ -0,0 +1,8 @@
+package src.trial_Deneme._OgrenciCozum;
+
+public class CopAdam {
+    public static void main(String[] args) {
+
+        System.out.println("   O"+"\n —————"+"\n   |"+"\n  / \\");
+    }
+}
