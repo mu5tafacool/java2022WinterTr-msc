8f64860b2125a3dcbbefb9181dba1c0e0d53a977
mu5tafacool <mustafakulf1@gmail.com>
2022-05-31 21:51:37 +0300

    31 05 22 21:55

---
+++ src/java_Batch_59_QA/day34_deneme/DenemeChild.java
@@ -0,0 +1,16 @@
+package src.java_Batch_59_QA.day34_deneme;
+
+import src.java_Batch_59_QA.day34_accessModifier_encapsulation.C01 ;
+
+public class DenemeChild extends C01 {
+
+    public static void main(String[] args) {
+
+        System.out.println(halkaAcikSayi);
+        halkaAcikSayi=30;
+
+        aileyeOzel=25;
+    }
+
+
+}
--- src/java_Batch_59_QA/day37_inheritance/deneme.java
+++ src/java_Batch_59_QA/day37_inheritance/deneme.java
@@ -1,4 +1,4 @@
-package day37_inheritance;
+package src.java_Batch_59_QA.day37_inheritance;
 
 public class deneme {
     public static void main(String[] args) {
