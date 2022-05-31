40631be4957db98f74ec1a1dfe44581ab0d8c9be
mu5tafacool <mustafakulf1@gmail.com>
2022-05-31 21:51:14 +0300

    31 05 22 21:55

---
+++ src/java_Batch_59_QA/day34_deneme/Deneme.java
@@ -0,0 +1,15 @@
+package src.java_Batch_59_QA.day34_deneme;
+
+import src.java_Batch_59_QA.day34_accessModifier_encapsulation.C01 ;
+
+public class Deneme {
+    public static void main(String[] args) {
+        // C01 obj = new C01();
+        // Const default access modifier'a sahip oldugundan
+        // baska package'dan kullanilamaz
+
+        C01.halkaAcikSayi=13;
+
+
+    }
+}
