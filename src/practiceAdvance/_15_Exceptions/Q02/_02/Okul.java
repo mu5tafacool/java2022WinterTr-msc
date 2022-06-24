5f5e3b9f77a849cab63b1c6c96abf4a76a57ceac
mu5tafacool <mustafakulf1@gmail.com>
2022-06-25 01:57:38 +0300

    25 06 22 00:30

---
+++ src/practiceAdvance/_15_Exceptions/Q02/_02/Okul.java
@@ -0,0 +1,39 @@
+package src.practiceAdvance._15_Exceptions.Q02._02;
+
+import java.util.ArrayList;
+
+public class Okul {
+
+    private String okulAd;
+    private int maxOgrenciSayisi;
+    private ArrayList<src.practiceAdvance._15_Exceptions.Q02._02.Ogrenci> ogrenciler = new ArrayList<>();
+
+    public Okul(String okulAd, int maxOgrenciSayisi) {
+        setOkulAd(okulAd);
+        setMaxOgrenciSayisi(maxOgrenciSayisi);
+    }
+
+    public String getOkulAd() {
+        return okulAd;
+    }
+
+    public void setOkulAd(String okulAd) {
+        this.okulAd = okulAd;
+    }
+
+    public int getMaxOgrenciSayisi() {
+        return maxOgrenciSayisi;
+    }
+
+    public void setMaxOgrenciSayisi(int maxOgrenciSayisi) {
+        this.maxOgrenciSayisi = maxOgrenciSayisi;
+    }
+
+    public ArrayList<src.practiceAdvance._15_Exceptions.Q02._02.Ogrenci> getOgrenciler() {
+        return ogrenciler;
+    }
+
+    public void setOgrenciler(ArrayList<src.practiceAdvance._15_Exceptions.Q02._02.Ogrenci> ogrenciler) {
+        this.ogrenciler = ogrenciler;
+    }
+}
