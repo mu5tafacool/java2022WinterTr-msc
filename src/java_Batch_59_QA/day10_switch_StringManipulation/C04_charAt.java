59c4cb49c7fda6784a9444fbe74e8db5dafa5aba
mu5tafacool <mustafakulf1@gmail.com>
2022-06-24 23:10:33 +0300

    24 06 22 23:30

--- src/java_Batch_59_QA/day10_switch_StringManipulation/C04_charAt.java
+++ src/java_Batch_59_QA/day10_switch_StringManipulation/C04_charAt.java
@@ -1,4 +1,6 @@
-package day10_switch_StringManipulation;
+package src.java_Batch_59_QA.day10_switch_StringManipulation;
+
+import java.util.Arrays;
 
 public class C04_charAt {
     public static void main(String[] args) {
@@ -21,6 +23,31 @@ public class C04_charAt {
 
         //son harfi yazdir
         System.out.println(str.charAt(14-1)); //uzunluk -1
+
+
+
+        //stringteki kelimedeki harfleri rakamlardan ayirma cevirme
+        //1.00₺
+        // replaceAll("\\D","")); //100
+        // replaceAll("[^a-zA-Z0-9.]","")); // ^ haric anlaminda // 1.00
+        // replaceAll("[a-zA-Z0-9]","")); // harf ve rakamlari kapsiyor //
+
+
+        String str1 = "ade1r4d3";
+        String arr[] = str1.split("");
+        System.out.println(Arrays.toString(arr));
+
+        int toplam = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (Character.isDigit(arr[i].charAt(0))) { //harften rakamlari ayirma
+                //karakter classindaki dijital method da
+                // i array in 0 inci karakterinin al
+                toplam += Integer.valueOf(arr[i]);
+            }
+        }
+        System.out.println("string ifadenin sayi degerleri toplami : " + toplam);
+
 
 
 
