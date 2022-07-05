d8019b45571a8b63d8489359c6d9669f90f22e43
mu5tafacool <mustafakulf1@gmail.com>
2022-07-06 01:05:02 +0300

    06 07 22 01:00

--- src/java_Batch_59_QA/day06_concatination_Operators/C01_Concatination.java
+++ src/java_Batch_59_QA/day06_concatination_Operators/C01_Concatination.java
@@ -4,24 +4,24 @@ public class C01_Concatination {
     public static void main(String[] args) {
 
 
-        String str1="Java";
-        String str2="Guzel";
-        int sayi1=5;
-        int sayi2=3;
+        String str1 = "Java";
+        String str2 = "Guzel";
+        int sayi1 = 5;
+        int sayi2 = 3;
 
 
         //java5guzel
-        System.out.println(str1+sayi1+str2);
+        System.out.println(str1 + sayi1 + str2);
 
         //2guzel15
-        System.out.println(sayi1-sayi2+str2+sayi1*sayi2);
+        System.out.println(sayi1 - sayi2 + str2 + sayi1 * sayi2);
 
         //java22
-        System.out.println(str1+(sayi1-sayi2)+(sayi1-sayi2));
+        System.out.println(str1 + (sayi1 - sayi2) + (sayi1 - sayi2));
 
         //53guzel
-        System.out.println(sayi1+(sayi2+str2));
-        System.out.println(""+sayi1+sayi2+str2);
+        System.out.println(sayi1 + (sayi2 + str2));
+        System.out.println("" + sayi1 + sayi2 + str2);
 
         /* **********************
         Eger tamamen sayilardan olusan bir String varsa ve biz bunu int'a cevirmek istersek
--- src/java_Batch_59_QA/day08_ifElseIfStatements/C01_IfElseStatements.java
+++ src/java_Batch_59_QA/day08_ifElseIfStatements/C01_IfElseStatements.java
@@ -1,14 +1,16 @@
 package src.java_Batch_59_QA.day08_ifElseIfStatements;
+
 import java.util.Scanner;
+
 public class C01_IfElseStatements {
     public static void main(String[] args) {
 
         // girilen bir karakterin, harf olup olmadigini bulalim
-        Scanner scan= new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("Lutfen bir karakter giriniz");
-        char karakter=scan.next().charAt(0);
+        char karakter = scan.next().charAt(0);
 
-        if ((karakter>='A' && karakter<='Z')  || (karakter>='a' && karakter<='z')){
+        if ((karakter >= 'A' && karakter <= 'Z') || (karakter >= 'a' && karakter <= 'z')) {
             System.out.println("Girdiginiz karakter bir harf");
         } else {
             System.out.println("Girdiginiz karakter harf degil");
