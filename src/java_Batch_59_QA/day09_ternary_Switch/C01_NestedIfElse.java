bf93fb3dfeb4a16a6b4bfcb5b24d56a2f6c43c70
mu5tafacool <mustafakulf1@gmail.com>
2022-07-10 21:49:33 +0300

    10 07 22 21:50

--- src/java_Batch_59_QA/day09_ternary_Switch/C01_NestedIfElse.java
+++ src/java_Batch_59_QA/day09_ternary_Switch/C01_NestedIfElse.java
@@ -12,16 +12,16 @@ public class C01_NestedIfElse {
         // Son rakamı 0 değil ise “5’e bölünen tek sayı” yazdırın.
         //Girdiği password 5’e bölünmüyorsa ekrana “Tekrar deneyin” yazdırın.
 
-        Scanner scan=new Scanner(System.in) ;
+        Scanner scan = new Scanner(System.in);
         System.out.println("lutfen 4 basamakli bir tamsayi girin");
-        int sayi= scan.nextInt();
+        int sayi = scan.nextInt();
 
 
-        if (sayi<1000 || sayi>9999){
+        if (sayi < 1000 || sayi > 9999) {
             System.out.println("Lutfen 4 basamakli sayi girin");
-        } else if (sayi%5==0) { //sayi 5 e tam bolunur
+        } else if (sayi % 5 == 0) { //sayi 5 e tam bolunur
             System.out.println();
-            if (sayi%10==0){ //4 basamakli 5 ile bolunebilen ve son rakami o olan
+            if (sayi % 10 == 0) { //4 basamakli 5 ile bolunebilen ve son rakami o olan
                 System.out.println("5 e bolunebilen cift sayi");
             } else { //4 basamakli 5 ile bolunebilen ve son rakami 0 olmayan
                 System.out.println("5 e bolunebilen tek sayi");
