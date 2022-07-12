ad2ed3852502fdd24dbacb74df1c31d978721b40
mu5tafacool <mustafakulf1@gmail.com>
2022-07-12 23:35:15 +0300

    12 07 22 23:50

--- src/java_Batch_59_QA/day06_concatination_Operators/C02_MantikOperatorleri.java
+++ src/java_Batch_59_QA/day06_concatination_Operators/C02_MantikOperatorleri.java
@@ -3,35 +3,34 @@ package src.java_Batch_59_QA.day06_concatination_Operators;
 public class C02_MantikOperatorleri {
     public static void main(String[] args) {
 
-        System.out.println(5+2==8); // false
+        System.out.println(5 + 2 == 8); // false
 
-        boolean sonuc1=5>4 && 7<9 && 6+3==9 && 5+2!=8;
-                        //t &&  t &&      t && t ; true
+        boolean sonuc1 = 5 > 4 && 7 < 9 && 6 + 3 == 9 && 5 + 2 != 8;
+        //t &&  t &&      t && t ; true
         System.out.println(sonuc1); //true
 
 
-        boolean sonuc2=5>4 && 7>9 && 6+3==9 && 5+2!=8;
-                        //t &&  f &&     t && t     ; false
+        boolean sonuc2 = 5 > 4 && 7 > 9 && 6 + 3 == 9 && 5 + 2 != 8;
+        //t &&  f &&     t && t     ; false
         System.out.println(sonuc2);
 
-        boolean sonuc3=5>4 & 7>9 & 6+3==9 & 5+2!=8;
+        boolean sonuc3 = 5 > 4 & 7 > 9 & 6 + 3 == 9 & 5 + 2 != 8;
 
         //&& kisa cozum yapip saglıyor & sonuna kadar bakiyor
 
 
-
-        int sayi3=15;
+        int sayi3 = 15;
 
         //sayi3 10 ile 20 arasi oldugunu true olarak goster
         // 10<sayi3<20 uclu karilastirmalar yapmaz java
         // System.out.println(10<sayi3<20); OLMAZZZZ
 
-        System.out.println(10<sayi3 && sayi3<20);
+        System.out.println(10 < sayi3 && sayi3 < 20); //true
 
 
-        int sayi4=5;
+        int sayi4 = 5;
         //sayi4 10 20 arasinda olmadigini gosterin
 
-        System.out.println(sayi4<10 || sayi4>20);
+        System.out.println(sayi4 < 10 || sayi4 > 20); //true
     }
 }
