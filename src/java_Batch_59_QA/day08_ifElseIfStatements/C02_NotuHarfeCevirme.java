bb48809a4d0c9479fb3957011a6072c88acf45ce
mu5tafacool <mustafakulf1@gmail.com>
2022-07-13 21:57:26 +0300

    13 07 22 21:50

--- src/java_Batch_59_QA/day08_ifElseIfStatements/C02_NotuHarfeCevirme.java
+++ src/java_Batch_59_QA/day08_ifElseIfStatements/C02_NotuHarfeCevirme.java
@@ -1,5 +1,7 @@
 package src.java_Batch_59_QA.day08_ifElseIfStatements;
+
 import java.util.Scanner;
+
 public class C02_NotuHarfeCevirme {
     public static void main(String[] args) {
         // Soru 7) Kullanicidan 100 uzerinden notunu isteyin.
@@ -10,15 +12,14 @@ public class C02_NotuHarfeCevirme {
         // =80’nin uzerinde ise “A”
         Scanner scan = new Scanner(System.in);
         System.out.println("Lutfen notunuzu giriniz");
-        double notSayi= scan.nextDouble();
-        if(notSayi<0 || notSayi>100){
+        double notSayi = scan.nextDouble();
+        if (notSayi < 0 || notSayi > 100) {
             System.out.println("Lutfen gecerli bir not giriniz");
-        }
-        else if (notSayi<50){
+        } else if (notSayi < 50) {
             System.out.println("notunuz D");
-        } else if (notSayi<60){
+        } else if (notSayi < 60) {
             System.out.println("notunuz C");
-        } else if(notSayi<80){
+        } else if (notSayi < 80) {
             System.out.println("Notunuz B");
         } else {
             System.out.println("Notunuz A");
