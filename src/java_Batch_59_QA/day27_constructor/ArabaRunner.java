9b868104836d42052c0c283333c062e7400486cc
mu5tafacool <mustafakulf1@gmail.com>
2022-07-16 00:09:18 +0300

    16 07 22 00:10

--- src/java_Batch_59_QA/day27_constructor/ArabaRunner.java
+++ src/java_Batch_59_QA/day27_constructor/ArabaRunner.java
@@ -1,4 +1,6 @@
-package src.java_Batch_59_QA.day26_forEachLoop_constructor;
+package src.java_Batch_59_QA.day27_constructor;
+
+import src.java_Batch_59_QA.day27_constructor.Araba;
 
 public class ArabaRunner {
     public static void main(String[] args) {
