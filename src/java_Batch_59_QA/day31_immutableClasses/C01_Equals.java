be3e389fd35d2c010cd697b03f65508edf331996
mu5tafacool <mustafakulf1@gmail.com>
2022-07-17 01:35:26 +0300

    17 07 22 01:10

--- src/java_Batch_59_QA/day31_immutableClasses/C01_Equals.java
+++ src/java_Batch_59_QA/day31_immutableClasses/C01_Equals.java
@@ -4,14 +4,14 @@ public class C01_Equals {
 
     public static void main(String[] args) {
 
-        String str1= "Eren";
-        String str2= "Eren"; //eger yeni bir obje olusturma veya bir islem yapma yoksa
+        String str1 = "Eren";
+        String str2 = "Eren"; //eger yeni bir obje olusturma veya bir islem yapma yoksa
         //Java string havuzunu kontrol eder. yani str1 ve str2 objeleri ayni olur bu durumda
 
-        String str3= new String("Eren");
+        String str3 = new String("Eren");
         //variable = degerdir  bu durumda yeni obje olusturu
 
-        String str4= str1+"";
+        String str4 = str1 + "";
         //st4 = str1 ama +"" concatinations oldugundan yeni bir obje olusturur
 
 
@@ -21,10 +21,9 @@ public class C01_Equals {
 
 
         // == hem degere hemde referansa bakar
-        System.out.println(str1==str2); // true
-        System.out.println(str1==str3); // false objeler farkli
-        System.out.println(str1==str4); // false objeler farkli
-
+        System.out.println(str1 == str2); // true
+        System.out.println(str1 == str3); // false objeler farkli
+        System.out.println(str1 == str4); // false objeler farkli
 
 
     }
