abc2b304af54378246f29addbca481123e64dd58
mu5tafacool <mustafakulf1@gmail.com>
2022-07-18 15:18:30 +0300

    18 07 22 15:10

--- src/java_Batch_59_QA/day33_varargs_stringBuilder/C01_Varargs.java
+++ src/java_Batch_59_QA/day33_varargs_stringBuilder/C01_Varargs.java
@@ -4,25 +4,25 @@ public class C01_Varargs {
     public static void main(String[] args) {
         // kac tane String verilirse verilsin
         // iclerinden en uzun olani yazdiran bir method olusturun
-        String str1="Ali";
-        String str2="Veli";
-        String str3="Oguzhan";
-        String str4="Samet";
+        String str1 = "Ali";
+        String str2 = "Veli";
+        String str3 = "Oguzhan";
+        String str4 = "Samet";
 
         // Yazilan argument sayisi sabitse herzamanki gibi bir method olusturabiliriz
         // ancak argument sayisinin degisme ihtimali varsa
         // o zaman varargs tercih edilir
-        enUzunKelime(str4,str2,str1,str3);
+        enUzunKelime(str4, str2, str1, str3);
 
     }
 
     private static void enUzunKelime(String... str) { //str varargs in str i
-        String enUzunStr="";
+        String enUzunStr = "";
 
-        for (String each: str
+        for (String each : str
         ) {
-            if (each.length()>enUzunStr.length()){
-                enUzunStr=each;
+            if (each.length() > enUzunStr.length()) {
+                enUzunStr = each;
             }
         }
 
