42f3eceb48cdfa907afe466fd782417414590d7d
mu5tafacool <mustafakulf1@gmail.com>
2022-07-22 00:07:32 +0300

    22 07 22 00:10

--- src/java_Batch_59_QA/day30_passByValue/C02_StaticBlocks.java
+++ src/java_Batch_59_QA/day30_passByValue/C02_StaticBlocks.java
@@ -11,15 +11,15 @@ public class C02_StaticBlocks {
         System.out.println("static olmayan blok calisti");
     }
 
-    static{
+    static {
         System.out.println("static blok calisti");
     }
 
     public static void main(String[] args) {
         System.out.println("main method basi");
 
-        C02_StaticBlocks obj1=new C02_StaticBlocks();
-        C02_StaticBlocks obj2=new C02_StaticBlocks();
+        C02_StaticBlocks obj1 = new C02_StaticBlocks();
+        C02_StaticBlocks obj2 = new C02_StaticBlocks();
 
 
     }
