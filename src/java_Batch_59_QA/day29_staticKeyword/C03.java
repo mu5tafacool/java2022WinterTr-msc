0128d104259dd98a54a940ef6129def3945a6b00
mu5tafacool <mustafakulf1@gmail.com>
2022-07-23 23:35:27 +0300

    23 07 22 23:10

--- src/java_Batch_59_QA/day29_staticKeyword/C03.java
+++ src/java_Batch_59_QA/day29_staticKeyword/C03.java
@@ -2,10 +2,10 @@ package src.java_Batch_59_QA.day29_staticKeyword;
 
 public class C03 {
 
-    static int sayi=5;
+    static int sayi = 5;
 
 
-    public static void artirma(){
+    public static void artirma() {
 
         sayi++;
         System.out.println("sayinin C03 deki degeri : " + sayi);
