b69cd7c1752d9b1bd74093d34c504e8ebc42462a
mu5tafacool <mustafakulf1@gmail.com>
2022-07-27 00:40:11 +0300

    27 07 22 00:10

--- src/java_Batch_59_QA/day26_forEachLoop_constructor/C03_ForEachLoop.java
+++ src/java_Batch_59_QA/day26_forEachLoop_constructor/C03_ForEachLoop.java
@@ -1,6 +1,8 @@
 package src.java_Batch_59_QA.day26_forEachLoop_constructor;
+
 import java.util.ArrayList;
 import java.util.List;
+
 public class C03_ForEachLoop {
     public static void main(String[] args) {
         // iki String array olusturunuz
@@ -8,25 +10,22 @@ public class C03_ForEachLoop {
         // Sonucu ekrana yazdiriniz.
         // Ortak eleman yoksa ekrana “Ortak eleman yok” yazdiriniz
 
-        String array1[] ={"Ali", "Ayse", "Can","Fatma"};
-        String array2[] ={"Can","Evren","Emsal", "Fatma", "Emre","Ali"};
+        String array1[] = {"Ali", "Ayse", "Can", "Fatma"};
+        String array2[] = {"Can", "Evren", "Emsal", "Fatma", "Emre", "Ali"};
 
-        List<String> ortakElemanlar=new ArrayList<>();
+        List<String> ortakElemanlar = new ArrayList<>();
 
-        for (String each1: array1
-        ) {
-            for (String each2:array2
-            ) {
-                if (each1.equals(each2)){
+        for (String each1 : array1) {
+            for (String each2 : array2) {
+                if (each1.equals(each2)) {
                     ortakElemanlar.add(each2);
                 }
             }
         }
 
-
-        if (ortakElemanlar.isEmpty()){
+        if (ortakElemanlar.isEmpty()) {
             System.out.println("Ortak eleman yok");
-        } else{
+        } else {
             System.out.println("Iki array'deki ortak elemanlar : " + ortakElemanlar);
         }
     }
