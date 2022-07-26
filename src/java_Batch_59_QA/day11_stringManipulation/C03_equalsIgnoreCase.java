e050e422e4ef5d3dd39697f8b181e18f2d9c514c
mu5tafacool <mustafakulf1@gmail.com>
2022-07-27 00:38:37 +0300

    27 07 22 00:10

--- src/java_Batch_59_QA/day11_stringManipulation/C03_equalsIgnoreCase.java
+++ src/java_Batch_59_QA/day11_stringManipulation/C03_equalsIgnoreCase.java
@@ -1,4 +1,4 @@
-package day11_stringManipulation;
+package src.java_Batch_59_QA.day11_stringManipulation;
 
 import java.util.Scanner;
 
@@ -10,14 +10,14 @@ public class C03_equalsIgnoreCase {
         // evet derse, cevabini ve  "derse katiliminiz onaylanmistir" yazdirin
         // hayir derse cevabini ve "Sonraki derslerimize bekleriz" yazdirin
 
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("Derse katilmak istermisiniz ? " +
                 "\n Evet veya Hayir yaziniz");
-        String cevap= scan.next();
+        String cevap = scan.next();
 
-        if (cevap.equalsIgnoreCase("evet")){
+        if (cevap.equalsIgnoreCase("evet")) {
             System.out.println("cevabiniz : " + cevap + " derse katiliminiz onaylanmistir");
-        } else if (cevap.equalsIgnoreCase("hayir")){
+        } else if (cevap.equalsIgnoreCase("hayir")) {
             System.out.println("cevabiniz : " + cevap + " Sonraki derslemize bekleriz");
         } else {
             System.out.println("lutfen evet veya hayir yaziniz");
