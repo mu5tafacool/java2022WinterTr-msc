bf5c2d531581f2abee98cf8fb0b139c552c67540
mu5tafacool <mustafakulf1@gmail.com>
2022-07-28 22:55:07 +0300

    28 07 22 23:00

--- src/java_Batch_59_QA/day17_forLoop/C03_ForLoop.java
+++ src/java_Batch_59_QA/day17_forLoop/C03_ForLoop.java
@@ -1,4 +1,4 @@
-package day17_forLoop;
+package src.java_Batch_59_QA.day17_forLoop;
 public class C03_ForLoop {
     public static void main(String[] args) {
         // Ekrana 10 kez “Java guzeldir” yazdirin
