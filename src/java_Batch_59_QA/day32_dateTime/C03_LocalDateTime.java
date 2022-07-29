affc070fc316264caf4dbd23836dcc73bd7fb5b8
mu5tafacool <mustafakulf1@gmail.com>
2022-07-29 23:51:47 +0300

    29 07 22 23:00

--- src/java_Batch_59_QA/day32_dateTime/C03_LocalDateTime.java
+++ src/java_Batch_59_QA/day32_dateTime/C03_LocalDateTime.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public class C03_LocalDateTime {
     public static void main(String[] args) {
 
-        LocalDateTime tarihSaat= LocalDateTime.now();
+        LocalDateTime tarihSaat = LocalDateTime.now();
         System.out.println(tarihSaat); // 2022-03-31T21:35:25.400865200
 
         System.out.println(tarihSaat.plusYears(3).plusMonths(2).plusDays(10).plusHours(5).plusMinutes(5));
