8aa230ee32699e19ba5963d2c301606b4a17bb11
mu5tafacool <mustafakulf1@gmail.com>
2022-07-29 23:50:59 +0300

    28 07 22 23:00

--- src/java_Batch_59_QA/day25_lists/C03_ListdenIstenmeyenElementleriSilme.java
+++ src/java_Batch_59_QA/day25_lists/C03_ListdenIstenmeyenElementleriSilme.java
@@ -1,20 +1,23 @@
 package src.java_Batch_59_QA.day25_lists;
+
 import java.util.ArrayList;
 import java.util.List;
+
 public class C03_ListdenIstenmeyenElementleriSilme {
     public static void main(String[] args) {
 
         // verilen bir array'den istenen elementi(kac tane varsa) silip, kalanlari
         // yeni bir array olarak yazdiran bir method olusturun
 
-        int arr[]={3,4,2,3,5,7,3,8,5,2,4};
-        int istenmeyenEleman=3;
-        istenmeyenElementiSilveYazdir(arr,istenmeyenEleman);
+        int arr[] = {3, 4, 2, 3, 5, 7, 3, 8, 5, 2, 4};
+        int istenmeyenEleman = 3;
+        istenmeyenElementiSilveYazdir(arr, istenmeyenEleman);
     }
+
     public static void istenmeyenElementiSilveYazdir(int[] arr, int istenmeyenEleman) {
-        List<Integer> yeniList=new ArrayList<>();
-        for (int i = 0; i <arr.length ; i++) {
-            if (arr[i] != istenmeyenEleman){
+        List<Integer> yeniList = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != istenmeyenEleman) {
                 yeniList.add(arr[i]);
             }
         }
