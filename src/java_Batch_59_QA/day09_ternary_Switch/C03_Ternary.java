400d2f8f4a9ff662b4ea7da07a06bde062953347
mu5tafacool <mustafakulf1@gmail.com>
2022-08-01 00:15:58 +0300

    01 08 22 00:10

--- src/java_Batch_59_QA/day09_ternary_Switch/C03_Ternary.java
+++ src/java_Batch_59_QA/day09_ternary_Switch/C03_Ternary.java
@@ -9,9 +9,9 @@ public class C03_Ternary {
         // ya bu sonucu direk yazdirmaliyiz
         // ya da sonucun data turune uygun bir variable'a  atama yapabiliriz
 
-        int sayi=-120;
+        int sayi = -120;
 
-        String sonuc =sayi>=100 ? "sayi 3 basamakli veya daha buyuk" : "sayi negatif veya 3 basamaktan kucuk";
+        String sonuc = sayi >= 100 ? "sayi 3 basamakli veya daha buyuk" : "sayi negatif veya 3 basamaktan kucuk";
 
         //Ternary bize sonuc dondurdugunden ya bu sonucu direk yazdirmali
         //ya da sonucun data turune uygun bir variable atama yapabiliriz
