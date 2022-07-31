d27485bf4f08504c85ebd393900dbe7b0625bc8f
mu5tafacool <mustafakulf1@gmail.com>
2022-07-31 23:39:52 +0300

    31 07 22 23:00

--- src/java_Batch_59_QA/day07_ifElseStatements/C03_TekCiftSayi.java
+++ src/java_Batch_59_QA/day07_ifElseStatements/C03_TekCiftSayi.java
@@ -1,23 +1,25 @@
 package src.java_Batch_59_QA.day07_ifElseStatements;
+
 import java.util.Scanner;
+
 public class C03_TekCiftSayi {
     public static void main(String[] args) {
         //Soru 1) Kullanicidan bir tamsayi isteyin
         // ve sayinin tek veya cift oldugunu yazdirin
         Scanner scan = new Scanner(System.in);
         System.out.println("Lutfen bir tam sayi giriniz");
-        int sayi= scan.nextInt();
-        if (sayi%2 == 0){
+        int sayi = scan.nextInt();
+        if (sayi % 2 == 0) {
             System.out.println("girilen sayi cift sayidir");
         }
-        if (sayi%2 != 0){
+        if (sayi % 2 != 0) {
             System.out.println("Girilen sayi tek sayi");
         }
         // normalde bir tamsayi ya tektir veya cifttir, ucuncu bir durum yoktur
         // o zaman tek olmasi ve cift olmasini iki ayri if ile degil
         // if else ile tek statement'da yapmak daha guzel olur
         // ======== if else ile cozum=======
-        if (sayi%2==0){
+        if (sayi % 2 == 0) {
             System.out.println("girdiginiz sayi cift sayidir");
         } else {
             System.out.println("girdiginiz sayi tek sayidir");
--- src/java_Batch_59_QA/day14_stringManipulation/C03_StringSayiToplama.java
+++ src/java_Batch_59_QA/day14_stringManipulation/C03_StringSayiToplama.java
@@ -1,4 +1,4 @@
-package day14_stringManipulation;
+package src.java_Batch_59_QA.day14_stringManipulation;
 
 public class C03_StringSayiToplama {
     public static void main(String[] args) {
