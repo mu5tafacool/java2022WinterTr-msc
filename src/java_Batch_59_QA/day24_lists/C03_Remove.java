b4c6a5b6cb3830ee6e09281536f7376e22f39e96
mu5tafacool <mustafakulf1@gmail.com>
2022-07-31 23:38:16 +0300

    31 07 22 23:00

--- src/java_Batch_59_QA/day24_lists/C03_Remove.java
+++ src/java_Batch_59_QA/day24_lists/C03_Remove.java
@@ -1,13 +1,15 @@
 package src.java_Batch_59_QA.day24_lists;
+
 import java.util.ArrayList;
 import java.util.List;
+
 public class C03_Remove {
     public static void main(String[] args) {
-        List<Integer> sayilar=new ArrayList<>();
+        List<Integer> sayilar = new ArrayList<>();
         sayilar.add(5); // [5]
         sayilar.add(3); // [5, 3]
-        sayilar.add(0,7); // [7, 5, 3]
-        sayilar.add(2,7); // [7, 5, 7, 3]
+        sayilar.add(0, 7); // [7, 5, 3]
+        sayilar.add(2, 7); // [7, 5, 7, 3]
         sayilar.remove(3); // index olarak 3 deki elementi siler
         // sayilar.remove(5); // IndexOutOfBoundsException
         /*
@@ -19,7 +21,7 @@ public class C03_Remove {
         // int sayi=5;
         // sayilar.remove(sayi); //IndexOutOfBoundsException
 
-        Integer sayi=5;  // Integer wrapper class'i kullaninca
+        Integer sayi = 5;  // Integer wrapper class'i kullaninca
         // sayi obje oldugundan, bu method calisti
 
         sayilar.remove(sayi);
