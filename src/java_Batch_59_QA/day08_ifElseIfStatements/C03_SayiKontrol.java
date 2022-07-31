256b7320a8ec79d00f08abb4d8f08224d5bec616
mu5tafacool <mustafakulf1@gmail.com>
2022-07-31 23:38:39 +0300

    31 07 22 23:00

--- src/java_Batch_59_QA/day08_ifElseIfStatements/C03_SayiKontrol.java
+++ src/java_Batch_59_QA/day08_ifElseIfStatements/C03_SayiKontrol.java
@@ -12,20 +12,20 @@ public class C03_SayiKontrol {
         // “farkli isaretlerde sayilarla islem yapamazsin” yazdirin,
         // sayilardan sifira esit olan varsa “sifir carpmaya gore yutan elemandir” yazdirin.
 
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("lutfen iki sayi giriniz");
 
-        double sayi1= scan.nextDouble();
-        double sayi2= scan.nextDouble();
-            // sayilarin ikisi de pozitif ise sayilarin toplamini yazdirin,
-        if (sayi1>0 && sayi2>0){
-            System.out.println("girdiginiz iki sayi pozitif oldugundan toplam " + (sayi1+sayi2) );
+        double sayi1 = scan.nextDouble();
+        double sayi2 = scan.nextDouble();
+        // sayilarin ikisi de pozitif ise sayilarin toplamini yazdirin,
+        if (sayi1 > 0 && sayi2 > 0) {
+            System.out.println("girdiginiz iki sayi pozitif oldugundan toplam " + (sayi1 + sayi2));
             // sayilarin ikisi de negative ise sayilarin carpimini yazdirin,
-        } else if (sayi1<0 && sayi2<0){
-            System.out.println("girdiginiz iki sayi negetif oldugundan carpimi " + (sayi1*sayi2) );
+        } else if (sayi1 < 0 && sayi2 < 0) {
+            System.out.println("girdiginiz iki sayi negetif oldugundan carpimi " + (sayi1 * sayi2));
             // sayilarin ikisi farkli isaretlere sahipse
             // “farkli isaretlerde sayilarla islem yapamazsin” yazdirin,
-        } else if (sayi1*sayi2<0){
+        } else if (sayi1 * sayi2 < 0) {
             System.out.println("girdiginiz iki sayi farkli oldugundan islem yapilmaz ");
 
         } else {
