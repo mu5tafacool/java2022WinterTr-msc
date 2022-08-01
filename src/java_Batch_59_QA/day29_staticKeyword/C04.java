5980c04bb0278581fa8f40d079c300b24bf1b3f0
mu5tafacool <mustafakulf1@gmail.com>
2022-08-02 00:46:08 +0300

    02 08 22 00:40

--- src/java_Batch_59_QA/day29_staticKeyword/C04.java
+++ src/java_Batch_59_QA/day29_staticKeyword/C04.java
@@ -4,10 +4,19 @@ public class C04 {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i <3 ; i++) {
+        for (int i = 0; i < 3; i++) {
             C05.eksiltme();
         }
 
         System.out.println(C03.sayi);
+    /*  c034
+        sayinin C03 deki degeri : 5
+        c034
+        sayinin C03 deki degeri : 5
+        c034
+        sayinin C03 deki degeri : 5
+        5
+    */
     }
+
 }
