e86698659b8c9a2f7c296f60e3298c08b6645a33
mu5tafacool <mustafakulf1@gmail.com>
2022-08-08 23:24:05 +0300

    08 08 22 23:40

--- src/java_Batch_59_QA/day29_staticKeyword/C05.java
+++ src/java_Batch_59_QA/day29_staticKeyword/C05.java
@@ -2,7 +2,7 @@ package src.java_Batch_59_QA.day29_staticKeyword;
 
 public class C05 {
 
-    public  static void eksiltme () {
+    public static void eksiltme() {
         C03.sayi--;
 
         System.out.println("c03" + C03.sayi);
