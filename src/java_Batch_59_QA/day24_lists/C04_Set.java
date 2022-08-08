ce69b7e85b78d465fea5c4467602386cf441febf
mu5tafacool <mustafakulf1@gmail.com>
2022-08-08 23:22:13 +0300

    08 08 22 23:40

--- src/java_Batch_59_QA/day24_lists/C04_Set.java
+++ src/java_Batch_59_QA/day24_lists/C04_Set.java
@@ -5,20 +5,20 @@ import java.util.List;
 
 public class C04_Set {
     public static void main(String[] args) {
-        List<String> isimler=new ArrayList<>();
+        List<String> isimler = new ArrayList<>();
         isimler.add("Aykut");
         isimler.add("Yusuf");
         isimler.add("Ilker");
         isimler.add("Oguzhan");
         System.out.println(isimler); //[Aykut, Yusuf, Ilker, Oguzhan]
 
-        isimler.set(1,"seckin");
+        isimler.set(1, "seckin");
         System.out.println(isimler);//[Aykut, seckin, Ilker, Oguzhan]
 
 
-        List<String> LogListesi=new ArrayList<>();
+        List<String> LogListesi = new ArrayList<>();
 
-        LogListesi.add(isimler.set(2,"Cosmos")); //ilkeri aldi sonra cosmos yapti. sonra ilkeri log listesine arsive ekledi
+        LogListesi.add(isimler.set(2, "Cosmos")); //ilkeri aldi sonra cosmos yapti. sonra ilkeri log listesine arsive ekledi
         System.out.println(isimler); //[Aykut, seckin, Cosmos, Oguzhan]
         System.out.println(LogListesi); //[Ilker] Arsiv aktarma tutma
     }
