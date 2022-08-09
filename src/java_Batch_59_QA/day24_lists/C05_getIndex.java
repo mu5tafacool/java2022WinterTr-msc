f912ce4b5c6ad3dda5a5ffc1ab18ddbc53e07cb0
mu5tafacool <mustafakulf1@gmail.com>
2022-08-09 23:11:25 +0300

    09 08 22 23:40

--- src/java_Batch_59_QA/day24_lists/C05_getIndex.java
+++ src/java_Batch_59_QA/day24_lists/C05_getIndex.java
@@ -5,11 +5,11 @@ import java.util.List;
 
 public class C05_getIndex {
     public static void main(String[] args) {
-        List<Integer> sayilarList= new ArrayList<>();
+        List<Integer> sayilarList = new ArrayList<>();
         sayilarList.add(5); // [5]
         sayilarList.add(3); // [5, 3]
-        sayilarList.add(0,7); // [7, 5, 3]
-        sayilarList.add(2,7); // [7, 5, 7, 3]
+        sayilarList.add(0, 7); // [7, 5, 3]
+        sayilarList.add(2, 7); // [7, 5, 7, 3]
         System.out.println(sayilarList.get(2)); // 7
         System.out.println(sayilarList); // [7, 5, 7, 3]
     }
