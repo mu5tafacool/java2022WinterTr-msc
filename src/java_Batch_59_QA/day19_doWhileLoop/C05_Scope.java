fbb6483f7f42c4e6f18bcbbc20145d25c35d6f40
mu5tafacool <mustafakulf1@gmail.com>
2022-08-17 00:15:26 +0300

    17 08 22

--- src/java_Batch_59_QA/day19_doWhileLoop/C05_Scope.java
+++ src/java_Batch_59_QA/day19_doWhileLoop/C05_Scope.java
@@ -1,4 +1,4 @@
-package day19_doWhileLoop;
+package src.java_Batch_59_QA.day19_doWhileLoop;
 public class C05_Scope {
 
     // 1- Bir method icerisinde olusturulan variable'lar sadece olusturulduklari
@@ -6,7 +6,7 @@ public class C05_Scope {
     // 2- Tum method'larin kullanabilmesini istedigimiz variable'lari
     //    Class level'da olustururuz
     //    Class level'da olusturdugumuz variable'i
-    //    -- static yaporsak tum methodlar kullanabilir
+    //    -- static yaparsak tum methodlar kullanabilir
     //    -- static olmazsa(instance) o zaman sadece static olmayan method'lar kullanabilir
     // 3- Loop icinde olusturulan variable'lar loop icerisinde kullanilabilir
     //    ama loop'un disinda kullanilamaz
