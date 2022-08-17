5602165f88b4b495d7ea3cdbdd0fcfc00392c38a
mu5tafacool <mustafakulf1@gmail.com>
2022-08-18 00:01:26 +0300

    18 08 22

--- src/java_Batch_59_QA/day07_ifElseStatements/C06_Emeklilik.java
+++ src/java_Batch_59_QA/day07_ifElseStatements/C06_Emeklilik.java
@@ -5,18 +5,16 @@ import java.util.Scanner;
 public class C06_Emeklilik {
     public static void main(String[] args) {
 
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("yasini gir");
-        int yas= scan.nextInt();
+        int yas = scan.nextInt();
 
-        if (yas>=65){
+        if (yas >= 65) {
             System.out.println("emekli olabilir");
         } else {
             System.out.println("emekli olamaz");
-            System.out.println(65-yas + " sene daha calismalisin");
+            System.out.println(65 - yas + " sene daha calismalisin");
         }
-
-
 
 
     }
