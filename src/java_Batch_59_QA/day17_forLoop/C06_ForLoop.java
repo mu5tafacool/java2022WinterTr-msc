9ddf8fd620ae23dc3721bf5b0b637a60852a7bc1
mu5tafacool <mustafakulf1@gmail.com>
2022-08-18 00:01:46 +0300

    18 08 22

--- src/java_Batch_59_QA/day17_forLoop/C06_ForLoop.java
+++ src/java_Batch_59_QA/day17_forLoop/C06_ForLoop.java
@@ -1,4 +1,4 @@
-package day17_forLoop;
+package src.java_Batch_59_QA.day17_forLoop;
 
 public class C06_ForLoop {
     public static void main(String[] args) {
@@ -11,7 +11,7 @@ public class C06_ForLoop {
 
         for (char i = harf1; i <=harf2 ; i++) {
             System.out.print(i + " ");
-
+            //A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \ ] ^ _ ` a b c d e f g h i j k l m n o p q r s t u v w x y z
         }
 
         // baslangic ve bitis sayilari arasinda artis miktari ile olusacak
@@ -22,7 +22,8 @@ public class C06_ForLoop {
         double artis=0.2; //i artis kullanimi
 
         for (double i = baslangic; i <=bitis ; i+=artis) {
-            System.out.print(i + " ");
+            System.out.print(i + " "); //
+            // 1.0 1.2 1.4 1.5999999999999999 1.7999999999999998 1.9999999999999998
         }
     }
 }
