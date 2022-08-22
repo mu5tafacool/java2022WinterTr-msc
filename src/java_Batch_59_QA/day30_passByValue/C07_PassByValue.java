c8f413f402037092e56fd6eaff4cfee57fb779bf
mu5tafacool <mustafakulf1@gmail.com>
2022-08-22 20:59:32 +0300

    22 08 22

--- src/java_Batch_59_QA/day30_passByValue/C07_PassByValue.java
+++ src/java_Batch_59_QA/day30_passByValue/C07_PassByValue.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 public class C07_PassByValue {
     public static void main(String[] args) {
 
-
         int arr[] = {1, 2, 3};
 
         System.out.println(Arrays.toString(arr)); //[1, 2, 3]
