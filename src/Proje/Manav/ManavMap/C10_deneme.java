f47cc2b74d4ec2dedd2adc44ddbb5a311b91fdb8
mu5tafacool <mustafakulf1@gmail.com>
2022-08-25 00:27:47 +0300

    25 08 22

---
+++ src/Proje/Manav/ManavMap/C10_deneme.java
@@ -0,0 +1,10 @@
+package src.Proje.Manav.ManavMap;
+
+public class C10_deneme {
+    public static void main(String[] args) {
+
+        double sayi = -20;
+
+        System.out.println(sayi > 0 ? "yari cap : " + 2 * sayi * 3.14 : "sayi sifirdan kucuk");
+    }
+}
