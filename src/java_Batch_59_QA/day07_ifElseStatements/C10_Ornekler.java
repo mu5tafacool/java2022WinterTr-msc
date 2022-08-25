47feb7615d36d3e6a75d09eb7981a41f2ead05d8
mu5tafacool <mustafakulf1@gmail.com>
2022-08-26 00:09:36 +0300

    26 08 22

--- src/java_Batch_59_QA/day07_ifElseStatements/C10_Ornekler.java
+++ src/java_Batch_59_QA/day07_ifElseStatements/C10_Ornekler.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class C10_Ornekler {
     public static void main(String[] args) {
 
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("lutfen dikdorgenin a uzunlugunu girin");
-        int aKenar= scan.nextInt();
+        int aKenar = scan.nextInt();
         System.out.println("lutfen dikdorgenin b uzunlugunu girin");
-        int bKenar= scan.nextInt();
+        int bKenar = scan.nextInt();
 
 
-        if (aKenar == bKenar){
+        if (aKenar == bKenar) {
             System.out.println("Girilen degerler KARE dir");
-        } else if (aKenar<0 || bKenar<0){
+        } else if (aKenar < 0 || bKenar < 0) {
             System.out.println("Girilen degerler NEGATİF deger olamaz");
         } else {
             System.out.println("Girilen degerler DİKDORTGEN dir");
