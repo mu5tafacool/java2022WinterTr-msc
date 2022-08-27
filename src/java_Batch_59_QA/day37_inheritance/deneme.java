336c723def2bf80846139128ce9d458ee08c1d9e
mu5tafacool <mustafakulf1@gmail.com>
2022-08-27 23:15:02 +0300

    27 08 22

--- src/java_Batch_59_QA/day37_inheritance/deneme.java
+++ src/java_Batch_59_QA/day37_inheritance/deneme.java
@@ -1,7 +1,5 @@
 package src.java_Batch_59_QA.day37_inheritance;
 
-public class deneme {
-    public static void main(String[] args) {
+public class deneme { public static void main(String[] args) {
         System.out.println("");
-    }
-}
+    }}
