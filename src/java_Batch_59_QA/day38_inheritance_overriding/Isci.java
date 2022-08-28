e03b53be762198811d6bca8b49dc292a16923ca2
mu5tafacool <mustafakulf1@gmail.com>
2022-08-28 23:16:32 +0300

    28 08 22

--- src/java_Batch_59_QA/day38_inheritance_overriding/Isci.java
+++ src/java_Batch_59_QA/day38_inheritance_overriding/Isci.java
@@ -1,18 +1,18 @@
 package src.java_Batch_59_QA.day38_inheritance_overriding;
 
-public class Isci extends Personel{
+public class Isci extends Personel {
 
-    public String statu="Isci";
-    public String haklar="Isciler Kidem tazminati alirlar";
-    public String ikramiye="Isciler Yilda bir ikramiye alir";
+    public String statu = "Isci";
+    public String haklar = "Isciler Kidem tazminati alirlar";
+    public String ikramiye = "Isciler Yilda bir ikramiye alir";
 
 
-    public void mesai(){
+    public void mesai() {
         System.out.println("Tum isciler haftalik 40 saat calisir");
     }
 
-    public void maasHesapla(){
+    public void maasHesapla() {
 
-        System.out.println("Tum isciler 30 gun * 8 saat * 11 euro = " + (30*8*11) + " Euro maas alir");
+        System.out.println("Tum isciler 30 gun * 8 saat * 11 euro = " + (30 * 8 * 11) + " Euro maas alir");
     }
 }
