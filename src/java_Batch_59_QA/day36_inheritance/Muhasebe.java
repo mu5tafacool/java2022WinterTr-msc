89d7962c28c5045e2df0475525de0de8b3ab1bc2
mu5tafacool <mustafakulf1@gmail.com>
2022-08-28 23:16:51 +0300

    28 08 22

--- src/java_Batch_59_QA/day36_inheritance/Muhasebe.java
+++ src/java_Batch_59_QA/day36_inheritance/Muhasebe.java
@@ -1,15 +1,16 @@
 package src.java_Batch_59_QA.day36_inheritance;
 
-public class Muhasebe extends Personel{
+public class Muhasebe extends Personel {
 
     protected int saatUcreti;
-    protected  String statu;
-    protected int maas=8;
+    protected String statu;
+    protected int maas = 8;
 
 
-    protected int maasHesapla(){
-        int maas=saatUcreti*8*30;
+    protected int maasHesapla() {
+        int maas = saatUcreti * 8 * 30;
         return maas;
     }
+
 
 }
