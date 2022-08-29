c07a74d5ce315ce398c1fe8b295efc547e67817c
mu5tafacool <mustafakulf1@gmail.com>
2022-08-29 23:24:47 +0300

    29 08 22

--- src/java_Batch_59_QA/day39_overriding/OverridingParent.java
+++ src/java_Batch_59_QA/day39_overriding/OverridingParent.java
@@ -2,11 +2,11 @@ package src.java_Batch_59_QA.day39_overriding;
 
 public class OverridingParent {
 
-    public void method1(){
+    public void method1() {
         System.out.println("Parent class method1");
     }
 
-    public void method2(){
+    public void method2() {
         System.out.println("Parent class method2");
     }
 
