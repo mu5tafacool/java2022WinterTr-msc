c71b326d588289acd6e36bdd84487e0261870553
mu5tafacool <mustafakulf1@gmail.com>
2022-09-10 20:19:41 +0300

    10 09 22

---
+++ src/practiceAdvance/_10_List/Q06.java
@@ -0,0 +1,58 @@
+package src.practiceAdvance._10_List;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Q06 {
+
+    public static void main(String[] args) {
+        /* TASK :
+         * Verilen arraydeki tekrar eden sayilari, ilki haric silip, tekrarsiz
+         * sayilardan olusan bir array haline getiren bir code create ediniz.
+         *
+         * INPUT : {1,2,2,3,1,4,2,5,6,8,7,5,9,1}
+         * OUTPUT : [1,2,3,4,5,6,7,8,9]
+         */
+        int[] arr = {1, 2, 2, 3, 1, 4, 2, 5, 6, 8, 7, 5, 9, 1};
+        List<Integer> tekrarsizList = new ArrayList<>();
+        for (Integer s : arr) {
+            if (!tekrarsizList.contains(s)) {//eleman yoksa add ile tekrarsız liste ekle eleman varsa  geç
+                tekrarsizList.add(s);
+            }
+        }
+        Collections.sort(tekrarsizList);//list elemanlrını NO sıraladık
+        int tekrarsizArray[] = new int[tekrarsizList.size()];//tekrarsizList.size() kadar lenght olan bos array atandı
+        for (int i = 0; i < tekrarsizList.size(); i++) {
+            tekrarsizArray[i] = tekrarsizList.get(i);
+
+        }
+        System.out.println(" ahanda sana her elemandan bir tane olan array ..." + Arrays.toString(tekrarsizArray));
+
+
+
+
+        int arr1[]={1, 2, 2, 3, 1, 4, 2, 5, 6, 8, 7, 5, 9, 1};
+        List<Integer>list1=new ArrayList<>();
+        for (Integer each:arr1
+             ) {
+            if (!list1.contains(each)){
+                list1.add(each);
+            }
+        }
+        Collections.sort(list1);
+        int arryeni[]=new int[list1.size()];
+
+        int i=0;
+        for (int l:list1
+             ) {  //forEach ile Array e deger atamasi
+          arryeni[i]= l;
+          i++;
+        }
+        System.out.println("\n****************\n\t2. yol\n"+ Arrays.toString(arryeni));
+    }
+
+}
+
