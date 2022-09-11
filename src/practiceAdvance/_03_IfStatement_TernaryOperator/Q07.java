08edd55157e7767655cf2ec0bfd282973baa5daa
mu5tafacool <mustafakulf1@gmail.com>
2022-09-11 17:22:03 +0300

    11 09 22

---
+++ src/practiceAdvance/_03_IfStatement_TernaryOperator/Q07.java
@@ -0,0 +1,33 @@
+package src.practiceAdvance._03_IfStatement_TernaryOperator;
+
+import java.util.Scanner;
+
+public class Q07 {
+
+	public static void main(String[] args) {
+		/* TASK :
+		 * Kullanicidan 1 harf (karakter) aliniz ve bunun buyuk harf, kucuk harf yada
+		   hatali giris seklinde code create ediniz
+		   * buyuk kucuk harf kontrolu
+		 */
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Lutfen bir karakter giriniz");
+
+		char ch = sc.next().charAt(0);
+
+		if (ch >= 'a' && ch <= 'z') {
+
+			System.out.println("Kucuk harf");
+
+		} else if (ch >= 'A' && ch <= 'Z') {
+			System.out.println("Buyuk harf");
+		} else {
+			System.out.println("Hatali giris yaptiniz.");
+		}
+
+
+	}
+
+}
