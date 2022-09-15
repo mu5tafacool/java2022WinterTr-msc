f1cacc37b42490ae78b81fb726829c487551f515
mu5tafacool <mustafakulf1@gmail.com>
2022-09-15 18:50:09 +0300

    15 09 22

---
+++ src/practiceAdvance/_05_StringManipulation/Q09.java
@@ -0,0 +1,26 @@
+package src.practiceAdvance._05_StringManipulation;
+
+import java.util.Scanner;
+
+public class Q09 {
+
+    public static void main(String[] args) {
+
+		/*  TASK :
+				StringMethods:
+				cift uzunlukta bir degiskenin ilk yarisini konsolda yazdirmak icin bir Java programi yaziniz.
+	    	    ORNEK:
+		     	INPUT      :  Python
+				OUTPUT :   Pyt
+	    */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen cift uzunlukta bir str giriniz :");
+        String str = scan.nextLine();
+        if (str.length() % 2 == 0) {                    //kelimenin ilk karakterinden yari uzunluguna kadar
+            System.out.println("strnin ilkyarisi:" + str.substring(0, str.length() / 2));
+        } else {
+            System.out.println("Lutfen cift uzunlukta bir str giriniz :");
+        }
+    }
+}
