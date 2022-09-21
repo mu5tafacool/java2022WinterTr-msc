a4e41e4c183fa469e30db404a61a8c6f74f41016
mu5tafacool <mustafakulf1@gmail.com>
2022-09-21 20:36:13 +0300

    21 09 22

---
+++ src/practiceAdvance/_08_While_DoWhile/Q14.java
@@ -0,0 +1,47 @@
+package src.practiceAdvance._08_While_DoWhile;
+
+import java.util.Scanner;
+
+public class Q14 {
+
+    public static void main(String[] args) {
+        /* TASK :
+        Verilen bir sayının faktöriyelini özyineli (recursive) olarak
+        hespalayan fonksiyonu yazınız.
+
+        Örnek Ekran Çıktıları
+        Bir sayi giriniz: 6*5*4*3*2*1=720
+        Faktöriyeli: 720
+
+        Bir sayı giriniz: 3*2*1
+        Faktöriyeli 6
+     */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("bir sayi giriniz : ");
+        int sayi = scan.nextInt();
+
+        int faktoriyel = 1;
+
+        while (sayi > 0) {
+            faktoriyel *= sayi;
+            sayi--;
+        }
+        System.out.println("faktoriyel :" + faktoriyel);
+
+
+        System.out.println("\n******************2yol**************");
+//mscool
+        System.out.print("rakam girin :");
+        int num = scan.nextInt();
+        int carpim = 1;
+        int toplam = 0;
+         while (carpim >= num){
+             toplam += carpim * carpim++;
+             carpim++;
+         }
+        System.out.println("toplam = " + toplam);
+    }
+}
+
+
