23d04c87bca4282f86cf0f958cfe29031cd462b2
mu5tafacool <mustafakulf1@gmail.com>
2022-09-28 00:35:37 +0300

    28 09 22

---
+++ src/Proje/P04_okulYönetimi/ogrcOgrtYonetimi/ogrcOgrtYonetimi01/Runner.java
@@ -0,0 +1,10 @@
+package src.Proje.P04_okulYönetimi.ogrcOgrtYonetimi.ogrcOgrtYonetimi01;
+
+
+import static src.Proje.P04_okulYönetimi.ogrcOgrtYonetimi.ogrcOgrtYonetimi01.Islemler.girisPaneli;
+
+public class Runner {
+    public static void main(String[] args) {
+        girisPaneli();//static method import edildi
+    }
+}
